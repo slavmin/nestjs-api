c6906071474cca500d739aa8035967edd1076d76
slavmin <49447696+slavmin@users.noreply.github.com>
2020-04-02 12:55:50 +0300

    Small bug fix

--- src/modules/auth/dto/register.dto.ts
+++ src/modules/auth/dto/register.dto.ts
@@ -1,4 +1,4 @@
-import { IsNotEmpty, IsEmail, IsAlphanumeric, MaxLength, MinLength } from 'class-validator';
+import { IsNotEmpty, IsEmail, IsAlphanumeric, MaxLength, MinLength, Matches } from 'class-validator';
 
 export class RegisterDto {
   @MinLength(2, { message: 'MIN:2' })
@@ -9,6 +9,7 @@ export class RegisterDto {
   email: string;
   @MinLength(6, { message: 'MIN:6' })
   @MaxLength(30, { message: 'MAX:30' })
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, { message: 'TOOWEAK' })
   password: string;
   @IsNotEmpty({ message: 'NOTEMPTY' })
   password_confirmation?: string;
